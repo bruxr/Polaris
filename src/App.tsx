363187dcbfad74328e590d18440598c98d799d23
Brux Romuar <brux.romuar@gmail.com>
2020-06-20 04:34:43 +0800

    Add initial router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { RecoilRoot } from 'recoil';
 import { ApolloProvider } from '@apollo/client';
+import { MemoryRouter as Router, Switch, Route }  from 'react-router-dom';
 
 import apollo from './services/apollo';
 import Dashboard from './views/Dashboard';
@@ -11,11 +12,15 @@ function App(): JSX.Element {
   return (
     <RecoilRoot>
       <ApolloProvider client={apollo}>
-        <div className="min-w-screen min-h-screen px-4">
-          <AuthGuard>
-            <Dashboard />
-          </AuthGuard>
-        </div>
+        <Router>
+          <div className="min-w-screen min-h-screen px-4">
+            <AuthGuard>
+              <Switch>
+                <Route path="/" component={Dashboard} exact />
+              </Switch>
+            </AuthGuard>
+          </div>
+        </Router>
       </ApolloProvider>
     </RecoilRoot>
   );
