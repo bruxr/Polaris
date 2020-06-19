548bcb1c6b847e40783c5b88dfa1f0319d29a314
Brux Romuar <brux.romuar@gmail.com>
2020-06-20 01:26:53 +0800

    Add initial linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['warn', 'always'],
+    'max-len': ['error', { code: 120 }],
+    'object-curly-spacing': ['warn', 'always'],
+  },
+};
