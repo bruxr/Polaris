93ded734599002c99deb89b8fc6349bd9d3c63b0
Brux Romuar <brux.romuar@gmail.com>
2020-06-24 01:41:19 +0800

    Extract views router to a separate comonent

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ import { MemoryRouter as Router, Switch, Route }  from 'react-router-dom';
 
 import apollo from './services/apollo';
 import Dashboard from './views/Dashboard';
-import AuthGuard from './components/AuthGuard';
 import Notifications from './views/Notifications';
+
+function Views(): JSX.Element {
+  return (
+    <Switch>
+      <Route path="/notifications" component={Notifications} />
+      <Route path="/" component={Dashboard} exact />
+    </Switch>
+  );
+}
 
 function App(): JSX.Element {
   return (
@@ -15,12 +23,7 @@ function App(): JSX.Element {
       <ApolloProvider client={apollo}>
         <Router>
           <div className="min-w-screen min-h-screen px-4">
-            <AuthGuard>
-              <Switch>
-                <Route path="/notifications" component={Notifications} />
-                <Route path="/" component={Dashboard} exact />
-              </Switch>
-            </AuthGuard>
+            <Views />
           </div>
         </Router>
       </ApolloProvider>
