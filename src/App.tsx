46c4e83a24e03bc9721fa8bdcd49d820c3a88cbb
Brux Romuar <brux.romuar@gmail.com>
2020-06-24 15:37:06 +0800

    Use BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ApolloProvider } from '@apollo/client';
-import { MemoryRouter as Router, Switch, Route }  from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route }  from 'react-router-dom';
 
 import Dashboard from './views/Dashboard';
 import AuthRoot from './components/AuthRoot';
