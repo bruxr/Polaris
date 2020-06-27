2e17ca19a9dafffa4d16964cb1672108ee560444
Brux Romuar <brux.romuar@gmail.com>
2020-06-27 23:26:23 +0800

    Add loading state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,6 +5,7 @@ import classnames from 'classnames';
 interface Props {
   type?: 'submit' | 'button',
   disabled?: boolean;
+  loading?: boolean;
   className?: string;
   onClick?: () => void,
 }
@@ -13,20 +14,22 @@ export default function Button({
   children,
   type,
   disabled,
+  loading,
   className,
   onClick,
 }: PropsWithChildren<Props>): JSX.Element {
   return (
     <button
       type={type || 'button'}
-      disabled={disabled || false}
+      disabled={disabled || loading || false}
       className={classnames(
         'block bg-black w-full p-4 text-white text-sm uppercase tracking-wider',
+        { 'opacity-25': loading },
         className,
       )}
       onClick={onClick}
     >
-      {children}
+      {loading ? 'Please wait...' : children}
     </button>
   );
 }
