01db4eef9ebdfc974608d11d660ead7766d09482
Brux Romuar <brux.romuar@gmail.com>
2020-07-26 19:48:43 +0800

    Add conditional signin

--- src/views/Signin/Signin.tsx
+++ src/views/Signin/Signin.tsx
@@ -3,16 +3,26 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Signin(): JSX.Element {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
-    <div className="flex min-w-full min-h-screen justify-center items-center">
-      <button
-        className="bg-blue-500 text-white rounded py-2 px-6"
-        onClick={() => { loginWithRedirect(); }}
-      >
-        Sign In
-      </button>
+    <div className="flex flex-col min-w-full min-h-screen justify-center items-center">
+      {isAuthenticated
+        ? (
+          <button
+            className="block py-2 px-6"
+            onClick={() => { logout(); }}
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            className="block bg-blue-500 text-white rounded py-2 px-6 mb-4"
+            onClick={() => { loginWithRedirect(); }}
+          >
+            Sign In
+          </button>
+        )}
     </div>
   );
 }
