7585b32ff6bb8072dd5b6b8cfea296ba3cda628b
Brux Romuar <brux.romuar@gmail.com>
2020-07-26 18:53:07 +0800

    Add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './assets/css/tailwind.out.css';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
