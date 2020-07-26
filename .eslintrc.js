172240bf0731e136a8a8f96783ba1cd29cc609b4
Brux Romuar <brux.romuar@gmail.com>
2020-07-26 18:30:54 +0800

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['warn', 'always'],
+    'max-len': ['error', { code: 120 }],
+    'object-curly-spacing': ['warn', 'always'],
+    },
+};
