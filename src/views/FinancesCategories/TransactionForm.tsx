8ff706e89852fcea3c16060ad0474c640065e708
Brux Romuar <brux.romuar@gmail.com>
2020-08-12 11:28:30 +0800

    Remove unused code

---
+++ src/views/FinancesCategories/TransactionForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TransactionForm = (): React.ReactElement => {
+  return (
+    
+  );
+};
+
+export default TransactionForm;
--- test/helpers.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-export async function getFixture(name: string): Promise<string> {
-  const contents = await fs.promises.readFile(`./test/fixtures/${name}.txt`, { encoding: 'utf8' });
-  return contents.toString();
-}
