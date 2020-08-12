a99865842d201fdf3d6d6afdcb4ea8274e10cb40
Brux Romuar <brux.romuar@gmail.com>
2020-08-12 13:34:22 +0800

    Catch checkbox types

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,19 +11,23 @@ type Props = FieldAttributes<Record<string ,unknown>> & {
   maxLength?: number;
 }
 
-export default function Input({ name, label, error, as, maxLength, children }: PropsWithChildren<Props>): JSX.Element {
+const Input = ({ type, name, label, error, as, maxLength, children }: PropsWithChildren<Props>): JSX.Element => {
   const { isSubmitting } = useFormikContext();
 
   return (
-    <div className="mb-4">
+    <div className={classnames('mb-4', { 'flex justify-between': type === 'checkbox' })}>
       <label htmlFor={name} className="block text-gray-700 mb-2">{label}</label>
       <Field
+        type={type}
         name={name}
         as={as}
         disabled={isSubmitting}
         className={classnames(
-          'block border border-gray-400 p-2 w-full mb-2',
-          { 'border-red-600': error }
+          'block border border-gray-400 p-2 mb-2',
+          {
+            'border-red-600': error,
+            'w-full': type !== 'checkbox',
+          },
         )}
         maxLength={maxLength}
       >
@@ -32,4 +36,6 @@ export default function Input({ name, label, error, as, maxLength, children }: P
       {error && <div className="text-red-600 text-xs">{error}</div>}
     </div>
   );
-}
+};
+
+export default Input;
