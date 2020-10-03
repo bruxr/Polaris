22b75bd21df840dfe1193f7357180f93faf9fc50
Brux Romuar <brux.romuar@gmail.com>
2020-10-04 02:03:24 +0800

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+/* eslint @typescript-eslint/no-var-requires:0 */
+/* eslint-env node */
+
+const colors = require('./src/colors.json');
+
 module.exports = {
   purge: [],
   theme: {
+    colors,
     extend: {},
   },
   variants: {},
