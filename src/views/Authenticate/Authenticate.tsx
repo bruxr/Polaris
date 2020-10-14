2893ebec5b15c190b15b0f358589daa2eef31128
Brux Romuar <brux.romuar@gmail.com>
2020-10-15 03:21:22 +0800

    Disable signups

--- src/views/Authenticate/Authenticate.tsx
+++ src/views/Authenticate/Authenticate.tsx
@@ -1,36 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import SigninForm from './SigninForm';
-import SignupForm from './SignupForm';
 
 export default function Authenticate(): React.ReactElement {
-  const [signingUp, setSigningUp] = useState(false);
-
   return (
     <div className="min-w-full min-h-screen p-5">
-      {signingUp ? (
-        <>
-          <SignupForm />
-          <button
-            type="button"
-            className="block w-full text-center mt-2 p-4"
-            onClick={() => setSigningUp(false)}
-          >
-            Sign In
-          </button>
-        </>
-      ) : (
-        <>
-          <SigninForm />
-          <button
-            type="button"
-            className="block w-full text-center mt-2 p-4"
-            onClick={() => setSigningUp(true)}
-          >
-            Sign Up
-          </button>
-        </>
-      )}
+      <SigninForm />
     </div>
   );
 }
