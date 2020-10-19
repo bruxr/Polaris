16d0344dac780cf353947236aba9cfd7a7393b25
Brux Romuar <brux.romuar@gmail.com>
2020-10-20 06:25:31 +0800

    Add temporary alert styles

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 
 export default function Alert({ children }: PropsWithChildren<unknown>): JSX.Element {
   return (
-    <div className="bg-red-100 border-red-600 text-red-600 mb-4 p-4 w-full">
+    <div className="border-red text-red mb-4 p-4 w-full">
       {children}
     </div>
   );
