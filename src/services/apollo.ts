9c93272be3ab3336e31f721323f944cac11d934e
Brux Romuar <brux.romuar@gmail.com>
2020-10-25 21:01:12 +0800

    Add auth header

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'https://graphql.fauna.com/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_FAUNADB_KEY}`,
+  },
 });
 
 export default client;
