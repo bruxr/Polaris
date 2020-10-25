088966e8627b6d430b1dfe39dc50e5dcbd4c2a5a
Brux Romuar <brux.romuar@gmail.com>
2020-10-25 16:26:02 +0800

    Add apollo

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/apm';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'typeface-roboto';
@@ -12,6 +13,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './index.css';
 
 import App from './App';
+import apolloClient from './services/apollo';
 import * as serviceWorker from './serviceWorker';
 
 // Setup sentry on non-dev environments
@@ -27,11 +29,13 @@ if (process.env.NODE_ENV !== 'development') {
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <Router>
-        <App />
-      </Router>
-    </RecoilRoot>
+    <ApolloProvider client={apolloClient}>
+      <RecoilRoot>
+        <Router>
+          <App />
+        </Router>
+      </RecoilRoot>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
