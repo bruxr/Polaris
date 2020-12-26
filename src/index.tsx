b0532708d35fa6d2ae99bb3e48e4465fe1201842
Brux Romuar <brux.romuar@gmail.com>
2020-12-27 02:13:16 +0800

    Disable sentry tracing for now

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     integrations: [
-      new Integrations.Tracing(),
+      // new Integrations.Tracing(),
     ],
     tracesSampleRate: 1.0,
   });
