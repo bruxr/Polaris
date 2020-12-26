6251706c70e0d8e75fa4a81afb9fbab7caa16a33
Brux Romuar <brux.romuar@gmail.com>
2020-12-27 03:24:32 +0800

    Load colors json to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const colors = require('./src/COLORS.json');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
     extend: {},
   },
   variants: {
