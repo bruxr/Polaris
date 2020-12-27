e501e49f8a11e76cecf0e47fa6d443a9ac374e90
Brux Romuar <brux.romuar@gmail.com>
2020-12-28 01:20:42 +0800

    Add datepicker component

---
+++ src/components/Datepicker/Datepicker.tsx
@@ -0,0 +1,56 @@
+import classnames from 'classnames';
+import format from 'date-fns/format';
+import isToday from 'date-fns/isToday';
+import React, { useMemo } from 'react';
+import parseISO from 'date-fns/parseISO';
+import isYesterday from 'date-fns/isYesterday';
+import { Field, useFormikContext } from 'formik';
+
+type Props = {
+  label: string,
+  name: string,
+}
+
+function Datepicker({ label, name }: Props): React.ReactElement { 
+  // We allow any here since we cannot fully determine the form values.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const formik = useFormikContext<any>();
+
+  const date = useMemo(() => {
+    const value = parseISO(formik.values[name]);
+    if (isToday(value)) {
+      return 'Today';
+    } else if (isYesterday(value)) {
+      return 'Yesterday';
+    } else {
+      return format(value, 'MMM d');
+    }
+  }, [formik.values, name]);
+
+  const hasError = useMemo(() => {
+    return !!formik.errors[name];
+  }, [formik.errors, name]);
+
+  return (
+    <div className="mb-4">
+      <label htmlFor={name} className="text-gray-500 text-sm mb-2 block">{label}</label>
+      <div
+        className={classnames(
+          'flex relative h-7 border-b pb-2',
+          hasError ? 'border-red text-red' : 'border-gray-500 text-white',
+        )}
+      >
+        <Field
+          name={name}
+          id={name}
+          type="date"
+          className="w-full absolute inset-0 opacity-0 z-10"
+        />
+        {date}
+      </div>
+      {hasError && <span className="inline-block mt-3 text-red text-xs">{formik.errors[name]}</span>}
+    </div>
+  );
+}
+
+export default Datepicker;
