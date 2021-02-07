f1b0afef45291f3ca9afb7481cdf648bf4769fa9
Brux Romuar <brux.romuar@gmail.com>
2021-02-07 18:27:36 +0800

    Fix variable name

--- src/db/transactions.test.ts
+++ src/db/transactions.test.ts
@@ -7,12 +7,12 @@ import { putTransaction } from './transactions';
 describe('putTransaction()', () => {
   it('should create a record', async () => {
     const attrs = await build(FactoryItem.Transaction);
-    const wallet = await putTransaction({
+    const transaction = await putTransaction({
       ...attrs,
       date: parseISO(attrs.date),
       createdAt: parseISO(attrs.createdAt),
     });
 
-    expect(wallet).not.toBeUndefined();
+    expect(transaction).not.toBeUndefined();
   });
 });
