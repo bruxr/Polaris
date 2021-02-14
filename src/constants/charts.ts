d3b869e1a49972d218b9ef4f1d337dc2667b1a65
Brux Romuar <brux.romuar@gmail.com>
2021-02-14 12:09:18 +0800

    Add additional colors

--- src/constants/charts.ts
+++ src/constants/charts.ts
@@ -1,14 +1,18 @@
 const CHART_COLORS = [
   // colors.red,
   // colors.green,
-  '#C026D3',
-  '#FDE047',
-  '#2563EB',
-  '#06B6D4',
-  '#9333EA',
-  '#84CC16',
-  '#F97316',
-  '#8B5CF6',
+  '#C026D3', // fuschia
+  '#8B5CF6', // violet
+  '#34D399', // emerald
+  '#FDE047', // yellow
+  '#2563EB', // blue
+  '#06B6D4', // cyan
+  '#9333EA', // purple 600
+  '#84CC16', // lime
+  '#F97316', // orange
+  '#8B5CF6', // purple 500
+  '#2DD4BF', // teal
+  '#EC4899', // pink
 ];
 
 export {
